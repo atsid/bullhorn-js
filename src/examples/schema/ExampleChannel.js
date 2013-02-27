a1f804239732747fbafdd35ecc6ac3055bf2fb22
kevin-convy-ats <kevin.convy@atsid.com>
2013-02-27 09:58:25 -0800

    Add simple example.

---
+++ src/examples/schema/ExampleChannel.js
@@ -0,0 +1,13 @@
+define({
+    "id": "schema/ExampleSchema",
+    "description": "A simple channel for testing",
+    "$schema": "http://json-schema.org/draft-03/schema",
+    "type": "object",
+    "properties": {
+        "text": {
+            "type": "string",
+            "description": "a property.",
+            "required": true
+        }
+    }
+});
