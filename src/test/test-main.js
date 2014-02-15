6260f47c67c02a2c0fd59ae5a77b7568bd95c464
talkswithnumbers <cloudbees@talkswithnumbers.com>
2014-02-14 16:10:50 -0800

    added karma/mocha and grunt and converted tests. removed jstd.

---
+++ src/test/test-main.js
@@ -0,0 +1,38 @@
+/*globals window, requirejs */
+"use strict";
+
+var allTestFiles = [],
+    pathToModule = function (path) {
+        var fixed = path.replace(/^\/base\//, '').replace(/\.js$/, '');
+        return fixed;
+    };
+
+Object.keys(window.__karma__.files).forEach(function (file) {
+    console.log(file);
+    if (/Test.*\.js$/.test(file)) {
+        // Normalize paths to RequireJS module names.
+        allTestFiles.push(pathToModule(file));
+    }
+});
+
+requirejs.config({
+    // Karma serves files from '/base'
+    baseUrl: '/base',
+
+    paths: {
+        'bullhorn': 'src/main',
+        'test': 'src/test'
+    },
+
+    map: {
+        "*": {
+            "bullhorn/Validator": "test/MockValidator"
+        }
+    },
+
+    // ask Require.js to load these files (all our tests)
+    deps: allTestFiles,
+
+    // start test run, once Require.js is done
+    callback: window.__karma__.start
+});
